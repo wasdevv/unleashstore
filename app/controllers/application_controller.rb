e0712c0e1deb4ba106c55d8bf4dc91b9db86bac6
wasdevv <contatxpriv@gmail.com>
2024-10-04 19:24:50 -0300

    config gems, and authenticate_user simple config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before :authenticate_user!
+
+  protected
+
+  def authenticate_user!
+    return unless current_user && !current_user.activated?
+
+    sign_out(current_user)
+    redirect_to new_user_session_path
+  end
 end
